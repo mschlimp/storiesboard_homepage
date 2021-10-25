ea72fa324ea243f35014193a5a46f66e7a365944
mschlimp <mschlimp@gmail.com>
2021-10-25 17:12:57 +0200

    first version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './landing/landing.component'
-
+import { ImprintComponent } from './imprint/imprint.component'
+import { PrivacyComponent } from './privacy/privacy.component'
+ 
 const routes: Routes = [
-    { path: '', component: LandingComponent }
+  { path: 'imprint', component: ImprintComponent},
+  { path: 'privacy', component: PrivacyComponent},
+  { path: '', component: LandingComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,20 @@ import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
 import {MatListModule} from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+import { CookieLawModule } from 'angular2-cookie-law';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingComponent } from './landing/landing.component';
 import { MatCardModule } from '@angular/material/card';
 import {MatExpansionModule} from '@angular/material/expansion';
 import { HeaderComponent } from './header/header.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
 
 
@@ -23,7 +27,9 @@ import { HeaderComponent } from './header/header.component';
   declarations: [
     AppComponent,
     LandingComponent,
-    HeaderComponent
+    HeaderComponent,
+    ImprintComponent,
+    PrivacyComponent
   ],
   imports: [
     BrowserModule,
@@ -36,8 +42,9 @@ import { HeaderComponent } from './header/header.component';
     MatButtonModule,
     MatMenuModule,
     MatIconModule,
-    FlexLayoutModule
-    
+    FlexLayoutModule,
+    MatGridListModule,
+    CookieLawModule
   ],
   providers: [],
   bootstrap: [AppComponent]
